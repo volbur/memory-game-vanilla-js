718be37e9f8aa7b0e13bbbd0195642d603734ab2
Volodymyr Burkovskyi <v.burkovskyi@gamzix.com>
2022-06-25 09:58:12 +0300

    [deck] created logic for save cards and shuffle, removeCard - methods

---
+++ deck.js
@@ -0,0 +1,28 @@
+/**
+ * Колода карт. Класс отвечает за создание и тасование карт. Содержит список изображений карт.
+ */
+
+class Deck {
+  #cardImages = ["apple.png", "cake.png", "car.png", "cat.png", "cherry.png", "factory.png",
+  "house.png", "plane.png", "ship.png", "train.png"];
+
+  constructor() {
+    this.cards = [];
+    this.#cardImages.forEach(image => {
+      this.cards.push(new Card(image));
+      this.cards.push(new Card(image));
+    });
+  }
+
+  shuffle() {
+    this.cards.sort(() => Math.random() - 0.5);
+  }
+
+  removeCard(card) {
+    let index = this.cards.findIndex(item => item.imagePath == card.imagePath);
+    if (index != -1) {
+      this.cards.splice(index, 1);
+      card.disconectFromDOM();
+    }
+  }
+}
